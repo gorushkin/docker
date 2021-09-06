c94b9197e34422b1fabbb22603b88742ce1e1590
gorushkin <gorushkin@gmail.com>
2021-09-06 20:46:19 +0300

    add express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('start');
+import express from 'express';
+
+const app = express();
+
+app.use('/', (_req, res) => {
+  res.send('Service is running!');
+});
+
+const PORT = 4000;
+
+app.listen(PORT, async () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+})
